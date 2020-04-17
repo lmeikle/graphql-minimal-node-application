8b98d320c76ce5a13226bfb94f168855cc2fe3eb
Laura Meikle <laura.s.meikle@gmail.com>
2020-04-17 15:23:06 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,61 @@
+import 'cross-fetch/polyfill'
 import 'dotenv/config';
+import ApolloClient, { gql } from 'apollo-boost';
 
-const userCredentials = { firstname: 'Robin' };
-const userDetails = { nationality: 'German' };
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  request: operation => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
+      },
+    });
+  },
+});
 
-const user = {
-  ...userCredentials,
-  ...userDetails,
-};
+const GET_REPOSITORIES_OF_ORGANIZATION = gql`
+  query($organization: String!) {
+    organization(login: $organization) {
+      name
+      url
+      repositories(first: 5) {
+        edges {
+          node {
+            id
+            name
+            url
+          }
+        }
+      }
+    }
+  }
+`;
 
-console.log(user);
+client
+  .query({
+    query: GET_REPOSITORIES_OF_ORGANIZATION,
+    variables: {
+      organization: 'the-road-to-learn-react',
+    }
+  })
+  .then(console.log);
 
-console.log(process.env.SOME_ENV_VARIABLE);
+const ADD_STAR = gql`
+  mutation AddStar($repositoryId: ID!) {
+    addStar(input: { starrableId: $repositoryId }) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`;
+
+client
+  .mutate({
+    mutation: ADD_STAR,
+    variables: {
+      repositoryId: 'MDEwOlJlcG9zaXRvcnk2MzM1MjkwNw==',
+    },
+  })
+  .then(console.log);
